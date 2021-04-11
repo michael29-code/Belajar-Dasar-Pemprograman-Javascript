8238821a734571d336ab6bf6104c452f02ed7a60
Michael <alfonsusmichael2003@gmail.com>
2021-04-11 17:05:54 +0700

    add ringkasan

---
+++ 6.Setup/index.js
@@ -0,0 +1,18 @@
+
+// npm init
+// menginisialisasikan Node.js
+
+// node namafile
+// menjalankan file 
+
+// npm run test lihat package.json
+// digunakan untuk menjalankan perintah agar cepat 
+
+
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
